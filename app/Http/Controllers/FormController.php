2326ba8a658c7a0caf1341a43e0a625aa00d278a
NahinAhmed28 <nahin.ahmed28@gmail.com>
2022-04-26 02:57:30 +0600

    first commit

---
+++ app/Http/Controllers/FormController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Form;
+use Illuminate\Http\Request;
+use PHPUnit\Util\Xml\Validator;
+use Symfony\Component\Console\Input\Input;
+
+class FormController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function index()
+    {
+        return view('public.form.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'phone' => 'required',
+            'subject' => 'required',
+            'message' => 'required',
+            'g-recaptcha-response' => 'required|captcha'
+        ]);
+
+
+
+        $value= Form::create([
+            'name'=> $request->name,
+            'email'=> $request->email,
+            'phone'=> $request->phone,
+            'subject'=> $request->subject,
+            'message'=> $request->message,
+            'g-recaptcha-response' => [
+                'required' => 'Please verify that you are not a robot.',
+                'captcha' => 'Captcha error! try again later or contact site admin.',
+            ],
+
+        ]);
+
+
+
+
+        if ($value)
+        {
+            return back()->with('success', 'Your form has been added!');
+        }
+        else {
+            return back()->with('danger', 'Your form has been failed to be added!');
+
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Form $form)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Form $form)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Form  $form
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Form $form)
+    {
+        //
+    }
+}
